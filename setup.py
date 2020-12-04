dbe036e91c2278733c02346e6aa0fa5d515b93ab
Lee <294622946@qq.com>
2020-12-04 16:21:43 +0800

    update requires

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 from setuptools import setup, find_packages
 
-install_requires = []
-with open('requirements.txt') as f:
-    for line in f.readlines():
-        line = line.strip()
-        if line:
-            install_requires.append(line)
-
 setup(
     name='dida',
-    version='0.0.0',
+    version='0.0.1',
     author="Lee",
     author_email="294622946@qq.com",
     url='https://github.com/Dog-Egg/dida',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=install_requires,
+    install_requires=['tornado', 'apscheduler', 'marshmallow'],
     entry_points={
         'console_scripts': ['dida=dida.cmdline:main']
     }
